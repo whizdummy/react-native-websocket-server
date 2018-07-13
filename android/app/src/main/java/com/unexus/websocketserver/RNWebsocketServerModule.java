4580ecd7f9808d784bb1ca5d097d52f9176bfcc9
Jerald John Pormon <jeraldjohnpormon@yahoo.com>
2018-07-14 01:26:55 +0800

    Instance Checking and Error Handling (Android)

--- android/app/src/main/java/com/unexus/websocketserver/RNWebsocketServerModule.java
+++ android/app/src/main/java/com/unexus/websocketserver/RNWebsocketServerModule.java
@@ -6,21 +6,8 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-
-import org.java_websocket.WebSocket;
-import org.java_websocket.WebSocketListener;
-import org.java_websocket.drafts.Draft;
-import org.java_websocket.exceptions.InvalidDataException;
-import org.java_websocket.framing.Framedata;
-import org.java_websocket.handshake.ClientHandshake;
-import org.java_websocket.handshake.Handshakedata;
-import org.java_websocket.handshake.ServerHandshake;
-import org.java_websocket.handshake.ServerHandshakeBuilder;
 
 /**
  * Created by umsi on 27/11/2017.
@@ -39,16 +26,29 @@ public class RNWebsocketServerModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void start(String ipAddress, int port) throws IOException, InterruptedException {
-        InetSocketAddress inetSocketAddress = new InetSocketAddress(ipAddress, port);
+    public void start(String ipAddress, int port) {
+        if (webServer == null) {
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(ipAddress, port);
 
-        webServer = new WebServer(inetSocketAddress);
+            webServer = new WebServer(inetSocketAddress);
 
-        webServer.start();
+            webServer.setReuseAddr(true);
+            webServer.start();
+        }
     }
 
     @ReactMethod
-    public void stop() throws IOException, InterruptedException {
-        webServer.stop();
+    public void stop() {
+        try {
+            if (webServer != null) {
+                webServer.stop();
+            }
+        } catch (IOException ioEx) {
+            Log.e("RNWS_IO_ERR", ioEx.getMessage());
+        } catch (InterruptedException iEx) {
+            Log.e("RNWS_INTERRUPT_ERR", iEx.getMessage());
+        } finally {
+            webServer = null;
+        }
     }
 }
